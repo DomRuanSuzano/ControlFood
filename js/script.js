2f26975b262e1a91cf9cedea8d4dfe34bdf6e427
DomRuanSuzano <dom.ruan.drs@gmail.com>
2021-12-01 14:44:54 -0300

    renomiei a pasta de imagens

--- js/script.js
+++ js/script.js
@@ -1,28 +1,11 @@
-document.addEventListener('DOMContentLoaded', function() { 
+document.addEventListener("DOMContentLoaded",function(){
 
- 
-    enviar=document.querySelector("#adiciona") 
-    enviar.addEventListener("click",function(event){
-        main=document.querySelector('main')
-        secao=document.createElement('section')
-        secao.classList.add("msg-resposta")
-        div=document.createElement('div')
-        div.classList.add("msg-titulo-invertida")
-        h2=document.createElement('h2')
-        h2.innerHTML="Dom Ruan"
-        conteudo= document.querySelector('.entrada-msg').value
-        div2=document.createElement("div")
-        div2.classList.add("bolha-resposta")
-        p=document.createElement('p')
-        p.innerHTML=conteudo
-        main.appendChild(secao)
-        secao.appendChild(div)
-        div.appendChild(h2)
-        secao.appendChild(div2)
-        div2.appendChild(p)
-        console.log(conteudo)
-        conteudo=document.querySelector('.entrada-msg')
-        conteudo.value = ''
-   }) 
+if (parseInt(localStorage.getItem('estoque_atual')) == NaN){
+    localStorage.setItem('estoque_atual',('48 unid.'))
+}
 
-}); 
+
+/*alterado = document.querySelector('#alterar')
+alterado.innerHTML = parseInt(localStorage.getItem('estoque_atual')) + parseInt(localStorage.getItem('adicionar')) + ' unid.'
+localStorage.setItem('estoque_atual',String(parseInt()))*/
+})
